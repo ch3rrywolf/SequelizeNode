0bd2e0f20f8e57c669d1f5117ec469f948529aba
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-07-06 14:59:55 +0100

    cnx server database init sequelize

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
+var bodyParser = require('body-parser')
+require('./models')
 const app = express()
+
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
     res.send('Hello World')
---
+++ models/index.js
@@ -0,0 +1,13 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('seqnode_db', 'root', 'pass', {
+    host: '127.0.0.1',
+    dialect: 'mysql'
+});
+
+try {
+    sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
